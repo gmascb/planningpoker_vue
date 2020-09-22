afac700e011cbec1dafed69d0e248314ea6208d5
Guilherme Mascarenhas Barbosa <gmascb@gmail.com>
2020-09-22 13:04:06 -0300

    Error: [VuetifyLoaderPlugin Error] No matching rule for vue-loader found.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify';
+import '@babel/polyfill'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
-createApp(App).use(router).mount('#app')
+// createApp(App)
+//     .use(router)
+//     .mount('#app')
+
+new Vue({
+    vuetify,
+    router,
+    render: h => h(App)
+}).$mount('#app');
